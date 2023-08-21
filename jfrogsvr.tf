dc552e261fb9b502dc551f88a524643aa2816abc
elaw2k4 <elaw2k2@gmail.com>
2023-08-21 18:41:47 -0400

    Create jfrogsvr.tf

---
+++ jfrogsvr.tf
@@ -0,0 +1,14 @@
+resource "aws_instance" "jfrog" {
+  #count         = "2"
+  ami           = data.aws_ami.amzlinux.id
+  instance_type = var.instance_type
+  key_name      = var.instance_keypair
+  user_data     = file("jfrog.sh")
+  vpc_security_group_ids      = [var.security_group_id] # Attach the specified security group
+  subnet_id                   = var.public_subnet_id    # Use the specified public subnet
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "wale-jfrog-svr"
+  }
+}
